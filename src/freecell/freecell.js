ce21361c03280e1ca672874797438b676d880031
Taeber Rapczak <taeber@rapczak.com>
2023-09-12 18:33:27 -0400

    Give attribution to comply with CC BY-SA 4.0

--- src/freecell/freecell.js
+++ src/freecell/freecell.js
@@ -223,6 +223,7 @@ var freecell = (function () {
     /**
      * Shuffles array in place. ES6 version
      * @param {Array} a items An array containing the items.
+     * @see https://stackoverflow.com/a/6274381
      */
     function shuffle(a) {
         for (let i = a.length - 1; i > 0; i--) {
